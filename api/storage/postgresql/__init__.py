f958a001e7f7ba3ff9edf361e6e9b6a8bd5834f5
Fabricio Alves <fabricio.alves@telavita.com.br>
2026-05-07 21:06:24 -0300

    fix: remove models export

--- api/storage/postgresql/__init__.py
+++ api/storage/postgresql/__init__.py
@@ -1,19 +1,4 @@
 from .base import Base
 from .session import engine, get_db
-from api.customers.models import Customer
-from api.offers.models import Offer
-from api.products.models import Product
-from api.carts.models import Cart, CartItem
-from api.checkouts.models import Checkout
 
-__all__ = [
-    "Base",
-    "engine",
-    "get_db",
-    "Checkout",
-    "Customer",
-    "Offer",
-    "Product",
-    "Cart",
-    "CartItem",
-]
+__all__ = ["Base", "engine", "get_db"]
